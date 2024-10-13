5148b3bc91a52e799c0e6a0aaf94c368f0f4d57f
Jeevan kumar <jeevankumarkorra2005@gmail.com>
2024-10-14 01:18:25 +0530

    changed firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDS3N2tQxJyqNiJd2m2qUI6hoFxcrOjkxA", 
-  authDomain: "warehouse-management-jeevs.firebaseapp.com",
-  projectId: "warehouse-management-jeevs",
-  storageBucket: "warehouse-management-jeevs.appspot.com",
-  messagingSenderId: "1003032067991",
-  appId: "1:1003032067991:web:7edc011b9ecba42de10ed2"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
